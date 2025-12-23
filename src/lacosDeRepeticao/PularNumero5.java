8a1207a5f7a27bbe90cec14bb1361c8a6cae1a00
Murilo Dutra <murilonunesdesouzadutra@hotmail.com>
2025-12-23 08:47:19 -0300

    Atividade PularNumero5 feita

---
+++ src/lacosDeRepeticao/PularNumero5.java
@@ -0,0 +1,33 @@
+package lacosDeRepeticao;
+
+import java.util.Scanner;
+
+public class PularNumero5 {
+
+    public static void main(String[] args) {
+        //Gustavo está desenvolvendo um jogo educativo de matemática e deseja um sistema que exiba os números de 1 até um valor definido pelo usuário, ignorando aqueles que terminam em 5. Essa mecânica tem o objetivo de tornar o jogo mais desafiador e imprevisível.
+        //
+        //Com base nesse cenário, crie um programa que conte de 1 até um número digitado pelo usuário, mas ignore números terminados em 5.
+        //
+        //Dica: Para saber se um número termina em 5, sempre dividimos ele por 10 e verificamos o resto da divisão. Se o resto for 5, significa que o número termina em 5.
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um numero maximo: ");
+        int numeroMax = scanner.nextInt();
+
+        for (int i = 0; i <= numeroMax; i++) {
+            if (i % 10 == 5) {
+                continue;
+            } else  {
+                System.out.print(i  + " ");
+
+            }
+
+
+        }
+        scanner.close();
+
+    }
+}
